f72d31c9911cb5c0d69921df4b618297ab01cf20
Heath Herrington <heathherrington@comcast.net>
2021-02-26 17:51:39 -0600

    pulled arrays from data for plotting

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,21 @@
-d3.json("data/samples.json").then((data) => {
-    console.log(data)});
+//Bring in json data using D3
+d3.json("data/samples.json").then((bbdata) => {
+    console.log(bbdata)
+
+ // Create arrays for plots
+ var bbsamples = bbdata.samples;
+ var otu_ids = bbsamples.map(bbsamples => {
+   return bbsamples.otu_ids;
+ });
+ var otu_labels = bbsamples.map(bbsamples => {
+   return bbsamples.otu_labels;
+ });
+ var otu_sample_values = bbsamples.map(bbsamples => {
+   return bbsamples.sample_values;
+ });
+ console.log(otu_ids);
+ console.log(otu_labels);
+ console.log(otu_sample_values)
+});
 
 
