725278cda49e8c2640a17fd25a5f9cf21c38980b
Heath Herrington <heathherrington@comcast.net>
2021-02-27 09:31:06 -0600

    having some issues with data in plot but added dropdown & plotting something

--- static/js/app.js
+++ static/js/app.js
@@ -2,48 +2,76 @@
 d3.json("data/samples.json").then((bbdata) => {
     console.log(bbdata)
 
- // Create arrays for plots
- var bbsamples = bbdata.samples;
- var otu_ids = bbsamples.map(bbsamples => {
-   return bbsamples.otu_ids;
- });
- var otu_labels = bbsamples.map(bbsamples => {
-   return bbsamples.otu_labels;
- });
- var otu_sample_values = bbsamples.map(bbsamples => {
-   return bbsamples.sample_values;
- });
- console.log(otu_ids);
- console.log(otu_labels);
- console.log(otu_sample_values);
+    // Create arrays for plots
+    var bbsamples = bbdata.samples;
+    var otu_ids = bbsamples.map(bbsamples => {
+        return bbsamples.otu_ids;
+    });
+    var otu_labels = bbsamples.map(bbsamples => {
+        return bbsamples.otu_labels;
+    });
+    var otu_sample_values = bbsamples.map(bbsamples => {
+        return bbsamples.sample_values;
+    });
+    console.log(otu_ids);
+    console.log(otu_labels);
+    console.log(otu_sample_values);
 
- // Slice arrays for plotting
- sliced_OTU_IDs = otu_ids.slice(0,10);
- sliced_OTU_Labels = otu_labels.slice(0,10);
- sliced_OTU_Sample_Values = otu_sample_values.slice(0,10);
- console.log(sliced_OTU_IDs);
- console.log(sliced_OTU_Labels);
- console.log(sliced_OTU_Sample_Values);
+    // bring otus into drop down menu using d3 select
+    var names = bbdata.names;
+    var dropdownMenu = d3.select("#selDataset");
+    names.forEach(name => {
+        dropdownMenu.append("option").text(name);
+    });
 
- // trace
- var trace1 = {
-    x: sliced_OTU_Sample_Values,
-    y: sliced_OTU_IDs,
-    text: sliced_OTU_Sample_Values,
-    type: "bar",
-    orientation: "h"
+
+    //create data points & slice data for top 10
+    var xvalues = [];
+    var yvalues = [];
+    var labelvalues = [];
+
+    otu_labels.forEach(array => {
+        var slice = array.slice(0, 10);
+        yvalues.push(slice);
+    });
+
+    otu_sample_values.forEach(array => {
+        var slice = array.slice(0, 10);
+        xvalues.push(slice);
+    });
+
+    otu_labels.forEach(array => {
+        var slice = array.slice(0, 10);
+        labelvalues.push(slice);
+    });
+
+    //console log to check
+    console.log(xvalues);
+    console.log(yvalues);
+    console.log(labelvalues);
+
+
+    //write trace for plot
+
+    // trace
+    var trace1 = {
+        x: xvalues[0].reverse(),
+        y: yvalues[0].reverse(),
+        text: labelvalues.reverse(),
+        type: "bar",
+        orientation: "h"
     };
     //data
     var barData = [trace1];
     //layout
     var layout = {
         margin: {
-            l:100,
-            r:100,
-            t:100,
-            b:100
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
         }
     };
-   Plotly.newPlot("bar", barData, layout) 
+    Plotly.newPlot("bar", barData, layout)
 
 })
